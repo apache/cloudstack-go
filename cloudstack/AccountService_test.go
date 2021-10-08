ad9c888737c7e5c8b6738338ea35633bd3599660
Pearl Dsilva <pearl1594@gmail.com>
2021-10-08 10:25:09 +0530

    refactor tests

--- cloudstack/AccountService_test.go
+++ cloudstack/AccountService_test.go
@@ -20,96 +20,36 @@
 package cloudstack
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
+func readData() (map[string]interface{} , error) {
+	var resp interface{}
+	apis, err := ioutil.ReadFile("AccountServiceTestData.json")
+	if err != nil {
+		return nil, err
+	}
+
+	if err := json.Unmarshal(apis, &resp); err != nil {
+		return nil, err
+	}
+
+	data := resp.(map[string]interface{})
+	return data, nil
+}
+
 func TestListAccounts(t *testing.T) {
 	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		resp := `
-{
-    "listaccountsresponse": {
-        "account": [
-            {
-                "accountdetails": {
-                    "key0": "value0",
-                    "key1": "value1"
-                },
-                "accounttype": 1,
-                "cpuavailable": "Unlimited",
-                "cpulimit": "Unlimited",
-                "cputotal": 1,
-                "domain": "ROOT",
-                "domainid": "62f5fe2e-5f5a-11e5-bc86-0242ac11180a",
-                "groups": [],
-                "id": "62f61a73-5f5a-11e5-bc86-0242ac11180a",
-                "ipavailable": "Unlimited",
-                "iplimit": "Unlimited",
-                "iptotal": 1,
-                "isdefault": true,
-                "memoryavailable": "Unlimited",
-                "memorylimit": "Unlimited",
-                "memorytotal": 512,
-                "name": "admin",
-                "networkavailable": "Unlimited",
-                "networklimit": "Unlimited",
-                "networktotal": 1,
-                "primarystorageavailable": "Unlimited",
-                "primarystoragelimit": "Unlimited",
-                "primarystoragetotal": 2,
-                "projectavailable": "Unlimited",
-                "projectlimit": "Unlimited",
-                "projecttotal": 1,
-                "receivedbytes": 100,
-                "secondarystorageavailable": "Unlimited",
-                "secondarystoragelimit": "Unlimited",
-                "secondarystoragetotal": 0,
-                "sentbytes": 100,
-                "snapshotavailable": "Unlimited",
-                "snapshotlimit": "Unlimited",
-                "snapshottotal": 0,
-                "state": "enabled",
-                "templateavailable": "Unlimited",
-                "templatelimit": "Unlimited",
-                "templatetotal": 0,
-                "user": [
-                    {
-                        "account": "admin",
-                        "accountid": "62f61a73-5f5a-11e5-bc86-0242ac11180a",
-                        "accounttype": 1,
-                        "apikey": "BFCyYeY0HhhrvNWC7FBwHTlso52ow3XWZUzNuiv03jmJOQn-5UH5BTVRVheiBnaRfYFE9yOzjwiqkbahq9P-Rw",
-                        "created": "2015-09-20T05:42:31+0000",
-                        "domain": "ROOT",
-                        "domainid": "62f5fe2e-5f5a-11e5-bc86-0242ac11180a",
-                        "email": "admin@mailprovider.com",
-                        "firstname": "Admin",
-                        "id": "62f6394b-5f5a-11e5-bc86-0242ac11180a",
-                        "iscallerchilddomain": false,
-                        "isdefault": true,
-                        "lastname": "User",
-                        "secretkey": "Em1UPdKTYEDaUxPXKtpqy_1uYluHWU2ZHotPi-VnBi_-MOEUAANoFHg3ycDProluDzYAXmT3YRssk367ylr_4A",
-                        "state": "enabled",
-                        "username": "admin"
-                    }
-                ],
-                "vmavailable": "Unlimited",
-                "vmlimit": "Unlimited",
-                "vmrunning": 1,
-                "vmtotal": 1,
-                "volumeavailable": "Unlimited",
-                "volumelimit": "Unlimited",
-                "volumetotal": 1,
-                "vpcavailable": "Unlimited",
-                "vpclimit": "Unlimited",
-                "vpctotal": 0
-            }
-        ],
-        "count": 1
-    }
-}
-		`
+		data, err := readData()
+		if err != nil {
+			return
+		}
+		resp := data["listAccountsResponse"]
 		fmt.Fprintln(w, resp)
 	}))
 	defer server.Close()
