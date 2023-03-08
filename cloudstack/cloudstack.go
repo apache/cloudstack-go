792e347dd3ce1ceec99f92e798f4dd28f13d3c15
poddm <8801231+poddm@users.noreply.github.com>
2023-03-08 14:23:59 -0800

    decode asterisk

--- cloudstack/cloudstack.go
+++ cloudstack/cloudstack.go
@@ -468,8 +468,9 @@ func (cs *CloudStackClient) newRawRequest(api string, post bool, params url.Valu
 	// * Serialize parameters, URL encoding only values and sort them by key, done by encodeValues
 	// * Convert the entire argument string to lowercase
 	// * Replace all instances of '+' to '%20'
-	// * Replace encoded asterisk (*) back to literal.  CloudStack’s internal encoder does not encode them; this results in an authentication failure for your API call.
-	//     http://docs.cloudstack.apache.org/projects/archived-cloudstack-getting-started/en/latest/dev.html)
+	// * Replace encoded asterisk (*) back to literal.
+	//    CloudStack’s internal encoder does not encode them; this results in an authentication failure for your API call.
+	//    http://docs.cloudstack.apache.org/projects/archived-cloudstack-getting-started/en/latest/dev.html)
 	// * Calculate HMAC SHA1 of argument string with CloudStack secret
 	// * URL encode the string and convert to base64
 	s := encodeValues(params)
